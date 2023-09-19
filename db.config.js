71bb781207ebb98073ca3a745ed2ff4388ed4006
SoonWin <145488381+SoonWin@users.noreply.github.com>
2023-09-19 20:17:31 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-ck4tefa9hnms739eukfg-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "9bzblkJrHQKvZxlvy4UlcLO6rJHv8uYG",
+    DB: "soonwinsrv",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+//
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
